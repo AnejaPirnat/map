f7e101469927f2d4878b002698624baac72f69dd
AnejaPirnat <aneja.pirnat@scv.si>
2023-01-09 13:43:20 +0100

    frontend

--- header.php
+++ header.php
@@ -1,4 +1,6 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+
+<link href = "css.css" rel = "stylesheet" />
+<nav class="navbar navbar-expand-lg navbar-light bg-light navbarcolor">
   <a class="navbar-brand"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-compass" viewBox="0 0 16 16">
   <path d="M8 16.016a7.5 7.5 0 0 0 1.962-14.74A1 1 0 0 0 9 0H7a1 1 0 0 0-.962 1.276A7.5 7.5 0 0 0 8 16.016zm6.5-7.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z"/>
   <path d="m6.94 7.44 4.95-2.83-2.83 4.95-4.949 2.83 2.828-4.95z"/>
@@ -10,13 +12,7 @@
     <ul class="navbar-nav">
       <li class="nav-item active">
         <a class="nav-link" href="index.php">Organiziraj potovanje <span class="sr-only"></span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" class = "right" href="login.php"> Prijava</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" class = "right" href="register.php">Registracija</a>
-      </li>
+        </li>
     </ul>
   </div>
 </nav>
--- index.php
+++ index.php
@@ -1,14 +1,22 @@
 <!DOCTYPE html>
-<html>
+<html lang="sl" class="h-100">
 <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>   
-        <?php
+        <link href = "css.css" rel = "stylesheet" />
+       <?php
           include "header.php";
         ?>
         <title>My Page</title>
 </head>
-<body>
+<body class="h-100">
+<div class="container h-100">
+<div class="row h-100 justify-content-center align-items-center">
+  <div class="col-10 col-md-8 col-lg-6">
+  <h1  style="text-align: center;"> Organiziraj svoje potovanje</h1>
+  <br>
     <h3> Kje želiš začeti potovanje? </h3>
     Iz katere države boš začel potovanje?
     <select class="form-select" aria-label="Default select example">
@@ -35,7 +43,7 @@ V katerem mestu boš začel potovanje?
         }
       ?>
 </select>
-
+<br>
 
     <h3>Kam želiš iti?</h3>
        V katero državo želiš potovati?
@@ -66,3 +74,10 @@ Koliko dni želiš potovati?
 <div class="form-outline">
   <input type="number" id="typeNumber" class="form-control" placeholder = "Trajanje potovanja (dnevi)" />
 </div>
+<br>
+<button type="button" class="btn btn-primary btncolor">Submit</button>
+</form>
+</div>
+</div>
+</div>
+</body>
